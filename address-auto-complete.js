d543a1a062a276bb14bc26317165ac4016f70298
tridentcreative <62360082+tridentcreative@users.noreply.github.com>
2023-05-30 15:19:49 +1000

    Update address-auto-complete.js

--- address-auto-complete.js
+++ address-auto-complete.js
@@ -1,4 +1,4 @@
-  document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function() {
     const input = document.getElementById('autoAddress');
     const options = {
       types: ['address']
