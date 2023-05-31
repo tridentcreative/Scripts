baca723629bdd264085ad84c2ccd22f5040a7ae3
tridentcreative <62360082+tridentcreative@users.noreply.github.com>
2023-05-31 15:02:35 +1000

    Update KILR_Verify.js

--- KILR_Verify.js
+++ KILR_Verify.js
@@ -1,8 +1,8 @@
 // JavaScript code
-document.getElementById('submitBtn').addEventListener('click', function() {
-  var input1Value = document.getElementById('input1').value;
-  var input2Value = document.getElementById('input2').value;
-  var logElement = document.getElementById('log');
+document.querySelector('[kilr_verify="submit"]').addEventListener('click', function() {
+  var input1Value = document.querySelector('[kilr_verify="input1"]').value;
+  var input2Value = document.querySelector('[kilr_verify="input2"]').value;
+  var logElement = document.querySelector('[kilr_verify="log"]');
 
   if (input1Value !== input2Value) {
     logElement.textContent = 'Your inputs don\'t match';
