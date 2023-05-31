654b30af5c8411a235905903f08f961d08e1a559
tridentcreative <62360082+tridentcreative@users.noreply.github.com>
2023-05-31 15:06:30 +1000

    Update KILR_Verify.js

--- KILR_Verify.js
+++ KILR_Verify.js
@@ -5,7 +5,7 @@ document.querySelector('[kilr_verify="submit"]').addEventListener('click', funct
   var logElement = document.querySelector('[kilr_verify="log"]');
 
   if (input1Value !== input2Value) {
-    logElement.textContent = 'Your inputs don\'t match';
+    logElement.textContent = 'Your Inputs Don\'t Match, Try Again.';
     logElement.style.visibility = 'visible';
   } else {
     logElement.style.visibility = 'hidden';
